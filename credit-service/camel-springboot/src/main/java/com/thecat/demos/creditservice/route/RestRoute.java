ccb2f0c35d6dd121e8d58a2f5ce1f7666b2f55c4
Felix Roberge <roberge.felix@gmail.com>
2021-11-23 00:15:05 -0500

    Fix deployment for the servie mesh in camel-springboot

---
+++ credit-service/camel-springboot/src/main/java/com/thecat/demos/creditservice/route/RestRoute.java
@@ -0,0 +1,65 @@
+package com.thecat.demos.creditservice.route;
+
+import com.thecat.demos.creditservice.entities.TransactionEntity;
+import com.thecat.demos.creditservice.services.CreditService;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestRoute  extends RouteBuilder {
+
+    private final Environment env;
+
+    public RestRoute(Environment env) {
+        this.env = env;
+    }
+
+    public void configure() throws Exception {
+        restConfiguration()
+                .contextPath(env.getProperty("camel.component.servlet.mapping.contextPath", "/rest/*"))
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "Spring Boot Camel Postgres Credit Service.")
+                .apiProperty("api.version", "1.0")
+                .apiProperty("cors", "true")
+                .apiContextRouteId("doc-api")
+                .port(env.getProperty("server.port", "8080"))
+                .bindingMode(RestBindingMode.auto);
+
+        rest("/credit")
+                .consumes(MediaType.APPLICATION_JSON_VALUE)
+                .produces(MediaType.APPLICATION_JSON_VALUE)
+                .get("/{clientId}").route()
+                .to("{{route.findTransactionByClientId}}")
+                .endRest()
+                .get("/").route()
+                .to("{{route.findAllTransactions}}")
+                .endRest()
+                .get("/version").route()
+                .to("direct:version")
+                .endRest()
+                .post("/").type(TransactionEntity.class).route()
+                .to("{{route.saveTransaction}}") 
+                .end();
+
+        from("{{route.saveTransaction}}")
+                .log("Received Body : ${body}")
+                .bean(CreditService.class, "addTransaction(${body})");
+
+        from("{{route.findTransactionByClientId}}")
+                .log( "Received header : ${header.clientId}")
+                .bean(CreditService.class, "findTransactionByClientId(${header.clientId})");
+
+        from("{{route.findAllTransactions}}")
+                .log( "Retrieve all transactions")
+                .bean(CreditService.class, "findAllTransactions");
+
+        from("direct:version")
+                .log("service healthy")
+                .transform().simple( "version1" );
+    
+    }
+}
